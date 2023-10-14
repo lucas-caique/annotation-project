40d0e6068e92e1421bb38a673704f49599892716
lucas-caique <lucascaique.nogueira@gmail.com>
2023-10-13 22:40:08 -0300

    Add loading functionality to imgvw
    Use ```-l``` to load an existing json annotation file. It's also
    posible to use ```-p``` and ```-l``` together to specify a different
    image folder (only works if files names are the same). Made that change
    to check the marked images against the annotated ones (the output of
    anntt.py)

--- imgvw/imgvw.py
+++ imgvw/imgvw.py
@@ -5,14 +5,14 @@ import cv2
 
 
 # color palette from: https://colorswall.com/palette/171300
-colors = [(47, 52, 227),
-          (63, 153, 246),
-          (74, 237, 255),
+colors = [(47,  52,  227),
+          (63,  153, 246),
+          (74,  237, 255),
           (114, 193, 56),
           (181, 192, 77),
           (220, 144, 51),
           (205, 116, 101),
-          (226, 97, 149),
+          (226, 97,  149),
           (155, 109, 246)]
 
 
@@ -62,29 +62,53 @@ class WorkingImages:
         return dict
 
 
+def draw_circle(img, x, y, clss):
+    radius = int(5 * (img.image.shape[1] / 1024))
+    cv2.circle(img.image, (x, y), radius, colors[clss], -1)
+
+
 def click_event(event, x, y, flags, imgs):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         cur_image = imgs.cur_image()
         cur_antt_class = cur_image.cur_antt_class
         cur_image.undo_stack.append((cur_image.image.copy(), cur_antt_class))
-        radius = int(5 * (cur_image.image.shape[1] / 1024))
-        cv2.circle(cur_image.image, (x, y), radius, colors[cur_antt_class], -1)
+        draw_circle(cur_image, x, y, cur_antt_class)
         cur_image.class_points.setdefault(cur_antt_class, []).append((x, y))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Annotate Images")
 
-    parser.add_argument("-p", help="path/to/image", type=str, required=True)
+    parser.add_argument("-p", help="path to images", type=str)
+    parser.add_argument("-l", help="load annotations", type=str)
     args = parser.parse_args()
 
     images = WorkingImages()
 
-    if os.path.isdir(args.p):
+    if args.l is None and os.path.isdir(args.p):
         for i in os.listdir(args.p):
             if i.endswith('.jpg') or i.endswith('.png'):
                 images.append(os.path.join(args.p, i))
-    elif os.path.isfile(args.p):
+
+    elif args.l is not None:
+        annotations = json.load(open(args.l))
+
+        path = args.l
+        if args.p is not None:
+            path = args.p
+
+        for imgs in annotations:
+            print(os.path.join(path, imgs))
+            images.append(os.path.join(path, imgs))
+            images.next()
+            for clss in annotations[imgs]:
+                for pts in annotations[imgs][clss]:
+                    cur_image = images.cur_image()
+                    cur_antt_class = cur_image.cur_antt_class
+                    draw_circle(cur_image, pts[0], pts[1], int(clss))
+                    cur_image.class_points.setdefault(cur_antt_class, []).append(pts)
+
+    elif args.p.endswith('.jpg') or args.p.endswith('.png'):
         images.append(args.p)
 
     cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
